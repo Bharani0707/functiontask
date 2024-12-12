c26473497ea324a85883a313d6e4c9d90de22220
Bharani0707 <bharani070712@gmail.com>
2024-12-12 13:02:19 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+function input(one){
+
+    var x= prompt("Enter Input press 1");
+    alert("Do you Want Continue yes!")
+    var x= prompt("Enter Input press 2");
+    alert("Do you Want Continue!")
+    var x= prompt("Enter Input press 3");
+    
+    
+
+
+    document.write("Input1: Large<br>")
+    document.write(" Input2: Medium <br>")
+    document.write(" Input3: Small<br>")
+    
+
+}
+input()
+
+
